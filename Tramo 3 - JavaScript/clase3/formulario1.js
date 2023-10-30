c513b6d196ffadfab48dd031aa138e129c87ed71
Herediaalejo <herediaalejodeveloper@gmail.com>
2023-10-30 00:36:41 -0300

    Ordeno repositorio

--- Tramo 3 - JavaScript/clase3/formulario1.js
+++
@@ -1,7 +0,0 @@
-function escribir(){
-    // Obtenemos lo que escribe el usuario en el campo de texto
-    var nombre = document.getElementById("nombre").value;
-
-    // Escribimos lo que obtuvimos en un elemento cuya id es respuesta
-    document.getElementById("respuesta").innerHTML = nombre.toUpperCase();
-}
--- Tramo 3 - JavaScript/clase5/05-06.js
+++
@@ -1,7 +0,0 @@
-function eliminarElemento(icono) {
-    // Obtener el <li> padre del icono de borrar
-    const listItem = icono.parentElement;
-    
-    // Eliminar el <li> del padre del icono
-    listItem.remove();
-}
